5a4a873d39833fd23afedc2bc2a6287e2fbd8b71
SebastianLischer <89944681+SebastianLischer@users.noreply.github.com>
2022-09-23 15:08:36 +0200

    Add project files.

---
+++ Projekt_M326/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Projekt_M326.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Projekt_M326
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+    }
+}
---
+++ Projekt_M326/ViewModel/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Input;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System;
+
+namespace Projekt_M326.ViewModel
+{
+    class UserViewModel
+    {
+        
+    }
+}
